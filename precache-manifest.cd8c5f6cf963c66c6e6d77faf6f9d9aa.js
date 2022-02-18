8ddbc66f6c2bbe9033edb31759d6eedbf95daaf0
Adhu-34 <adhuadarshkdr@gmail.com>
2022-02-18 20:38:13 +0530

    Updates

---
+++ precache-manifest.cd8c5f6cf963c66c6e6d77faf6f9d9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c08797a5121c695c56dfaebd17d723",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "9245e6b1844751f5c04a",
+    "url": "/react-demo/static/css/main.68e73e61.chunk.css"
+  },
+  {
+    "revision": "e831656e1ddc8eba9742",
+    "url": "/react-demo/static/js/2.d3ad92b1.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/react-demo/static/js/2.d3ad92b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9245e6b1844751f5c04a",
+    "url": "/react-demo/static/js/main.f4f3b576.chunk.js"
+  },
+  {
+    "revision": "e2fe78b2384172e638d4",
+    "url": "/react-demo/static/js/runtime-main.07636b1b.js"
+  }
+]);
